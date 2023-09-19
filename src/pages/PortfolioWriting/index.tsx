af24694580a113a0c6638b78b83fa01840fb4bed
saem <saemilee01@gmail.com>
2023-09-19 21:02:11 +0900

    refactor: 로그인 확인 관련 로직 페이지로 이동

--- src/pages/PortfolioWriting/index.tsx
+++ src/pages/PortfolioWriting/index.tsx
@@ -1,6 +1,18 @@
+import { useNavigate } from 'react-router-dom';
 import PortfolioWritingForm from '../../components/PortfolioWritingForm/PortfolioWritingForm';
+import { useEffect } from 'react';
+import ROUTES from '../../constants/Routes';
+import * as token from '../../apis/Token';
 
 function PortfolioWriting() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token.getToken()) {
+      alert('로그인 후 사용 가능합니다.');
+      navigate(ROUTES.LOGIN);
+    }
+  }, [navigate]);
   return <PortfolioWritingForm />;
 }
 
