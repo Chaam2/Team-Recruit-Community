30866ce5668e5f944bb5de5d383d0d79ee461643
saem <saemilee01@gmail.com>
2023-09-19 13:59:39 +0900

    fix: 에러시 로딩 false로 변경

--- src/hooks/controllers/useProjectList.ts
+++ src/hooks/controllers/useProjectList.ts
@@ -35,6 +35,7 @@ function useProjectList() {
       } catch (e) {
         setProjectList((prev) => ({
           ...prev,
+          isLoading: false,
           isError: true,
         }));
       }
